5140161590f9d8d75c222cca5b8b9edcd0b17bb8
Jerry Guerrero <jerry1996714@gmail.com>
2022-03-08 11:20:38 -0800

    addding ids and event listeners for the buttons

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+var SubmitButton = document.querySelector("#submit")
+var SubmitButton2 = document.querySelector("#button2")
+var ResultsPage = "./search-results.html"
+
+SubmitButton.addEventListener("click",function() {
+    document.location.replace(ResultsPage)
+})
+
+SubmitButton2.addEventListener("click",)
